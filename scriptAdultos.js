f6e29f1e23e2ecd805e5cf0a943efd31f675f2bb
edwin1327 <edwinfabian.13@gmail.com>
2024-11-12 09:50:29 -0500

    Productos Agregados 20241112

--- scriptAdultos.js
+++ scriptAdultos.js
@@ -11,7 +11,23 @@ const products = [
     {name: 'Anillo vibrador para hombre', reference: 'P-0040', image: 'img/img036.png', quantity: 2, price: 10000, category: 'juguetes'},
     {name: 'Dildo Realista Silicona 15 cm', reference: 'P-0041', image: 'img/img037.png', quantity: 3, price: 25000, category: 'dildos'},
     {name: 'Parques Erótico - Juego de Mesa', reference: 'P-0042', image: 'img/img038.png', quantity: 2, price: 30000, category: 'juguetes'},
-        
+    {name: 'Babydoll sexy tipo corse con liguero, esposas y medias', reference: 'P-0043', image: 'img/img039.png', quantity: 4, price: 25000, category: 'babydolls'},
+    {name: 'Babydoll sexy en V enterizo con liguero y medias', reference: 'P-0044', image: 'img/img040.png', quantity: 3, price: 20000, category: 'babydolls'},
+    {name: 'Babydoll Sexy en V  en encaje', reference: 'P-0045', image: 'img/img041.png', quantity: 7, price: 15000, category: 'babydolls'},
+    {name: 'Babydoll Sexy en V  en encaje', reference: 'P-0046', image: 'img/img042.png', quantity: 5, price: 15000, category: 'babydolls'},
+    {name: 'Babydoll sexy lolita erótica', reference: 'P-0047', image: 'img/img043.png', quantity: 3, price: 40000, category: 'babydolls'},
+    {name: 'Babydoll Sexy Monjita', reference: 'P-0048', image: 'img/img044.png', quantity: 2, price: 40000, category: 'babydolls'},
+    {name: 'Babydoll Sexy Enfermera', reference: 'P-0049', image: 'img/img045.png', quantity: 1, price: 40000, category: 'babydolls'},
+    {name: 'Cubre Pezones - Gato', reference: 'P-0050', image: 'img/img046.png', quantity: 4, price: 10000, category: 'juguetes'},
+    {name: 'Cubre Pezones - Corazón', reference: 'P-0051', image: 'img/img047.png', quantity: 5, price: 10000, category: 'juguetes'},
+    {name: 'Tanga invisible de encaje con flores - Rojo', reference: 'P-0052', image: 'img/img048.png', quantity: 6, price: 15000, category: 'babydolls'},
+    {name: 'Masturbador Artificial para hombre (Boca) - Sin vibración', reference: 'P-0053', image: 'img/img049.png', quantity: 3, price: 45000, category: 'juguetes'},
+    {name: 'Masajeador vibrador Electra Wand', reference: 'P-0054', image: 'img/img050.png', quantity: 2, price: 70000, category: 'juguetes'},
+    {name: 'Medias tipo liguero - Cindy Love S4816', reference: 'P-0055', image: 'img/img051.png', quantity: 6, price: 15000, category: 'babydolls'},
+    {name: 'Medias tipo liguero - Cindy Love S4535', reference: 'P-0056', image: 'img/img052.png', quantity: 6, price: 15000, category: 'babydolls'},
+    {name: 'Babydoll Tipo Vestido Sexy en Encaje Sensual', reference: 'P-0057', image: 'img/img053.png', quantity: 4, price: 18000, category: 'babydolls'},
+    {name: 'Babydoll Spandex sexy con encaje', reference: 'P-0058', image: 'img/img054.png', quantity: 4, price: 18000, category: 'babydolls'},
+    
     // Agrega aquí los demás productos con sus respectivas categorías (joyeria, accesorios, ropa, etc.)
 ];
 
